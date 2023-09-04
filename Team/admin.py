10434b4054688ce7bff07c291720d805bea2dfc3
Vibhor Singh <v2013singh@gmail.com>
2023-09-05 00:43:12 +0530

    TeamData

---
+++ Team/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import TeamData
+
+admin.site.register(TeamData) 
---
+++ Team/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import TeamData
+from .serializers import TeamDataSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+#view to get overall team data
+@api_view(['GET'])
+def teamData(request):
+    teammembers = TeamData.objects.all()
+    rtr = TeamDataSerializer(teammembers, many = True)
+    return Response(rtr.data)
+
+#view to get each year's team data
+@api_view(['GET'])
+def teamDataByYear(request, year):
+    teammembers = TeamData.objects.all().filter(year = year)
+    rtr = TeamDataSerializer(teammembers, many = True)
+    return Response(rtr.data)
+
