dfe0118dac498259c98cf78bbf8e3dec782f2524
Viraj Chandra <virajchandra51@gmail.com>
2023-09-05 03:28:24 +0530

    build ip

--- Team/settings.py
+++ Team/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static")
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') => vercel
-STATIC_ROOT =os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
