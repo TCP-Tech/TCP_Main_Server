28de64d64fce8796338a5ebdd02863536908b531
Vibhor Singh <v2013singh@gmail.com>
2023-09-07 21:04:17 +0530

    Image Url Added

--- Events/serializers.py
+++ Events/serializers.py
@@ -3,6 +3,9 @@ from .models import Event
 
 #Serializer for Events
 class EventSerializer(serializers.ModelSerializer):
+    img_url = serializers.SerializerMethodField('get_image_url')
     class Meta:
         model = Event
-        fields = ['year','title','date','img','description','instaLink']
+        fields = ['year','title','date','img','description','instaLink','img_url']
+    def get_image_url(self, obj):
+        return obj.img.url
