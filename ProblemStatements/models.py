f361a514da7704173ed4cb8f9f64b9d07e8aa741
Vibhor Singh <v2013singh@gmail.com>
2023-11-09 01:15:08 +0530

    final

--- ProblemStatements/models.py
+++ ProblemStatements/models.py
@@ -6,9 +6,11 @@ class ProblemStatement(models.Model):
     
     year = models.CharField(max_length=5)
     title = models.CharField(max_length=50)
-    domain = models.CharField(max_length=50)
-    img = models.ImageField(upload_to="problemStatements/", null = True, blank=True)
+    domain = models.CharField(max_length=50)  
+    background = models.TextField(max_length=2000, blank=True)
     statement = models.TextField(max_length=2000, blank=True)
+    impact = models.TextField(max_length=2000, blank=True)
+    img = models.ImageField(upload_to="problemStatements/", null = True, blank=True)
     
 
     @property
