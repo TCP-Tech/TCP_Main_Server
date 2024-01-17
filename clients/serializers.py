100352f967cbc108611ecc5858d55d6a4ba769f8
Ayush shukla <ayushshukla3999@gmail.com>
2024-01-18 00:22:16 +0530

    topic wise count added

--- clients/serializers.py
+++ clients/serializers.py
@@ -5,7 +5,7 @@ class MentorSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Mentor
-        fields = ['name', 'username', 'password', 'branch', 'semester', 'phone_number', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID', 'score', 'total_q']
+        fields = '__all__'
 
 class MenteeSerializer(serializers.ModelSerializer):
     
@@ -17,11 +17,11 @@ class MenteeSerializer(serializers.ModelSerializer):
 class MenteeUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentee
-        fields = ['image', 'name', 'phone_number', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
+        fields = ['image', 'name', 'phone_number','email', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
 class MentorUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentor
-        fields = ['image', 'name', 'phone_number', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
+        fields = ['image', 'name', 'phone_number','email', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
