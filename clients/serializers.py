d61da20fb6acc92e8217404114c26f190a639385
Praveen Patro <praveen.patro911@gmail.com>
2025-02-01 02:00:36 +0530

    fixes

--- clients/serializers.py
+++ clients/serializers.py
@@ -19,23 +19,34 @@ class MenteeSerializer(serializers.ModelSerializer):
         fields = '__all__'
     
 #['name','email', 'username', 'password', 'branch', 'semester', 'phone_number', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID', 'score', 'total_q']
+class MentorGetSerializer(serializers.ModelSerializer):
+    Mentorteam = TeamDetailSerializer(read_only=True)
+    class Meta:
+        model = Mentor
+        exclude = ['email', 'password','phone_number']
 
+class MenteeGetSerializer(serializers.ModelSerializer):
+    Menteeteam = TeamDetailSerializer(read_only=True)
+    class Meta:
+        model = Mentee
+        exclude = ['email', 'password','phone_number']
+    
 class MenteeUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentee
         fields = ['image','mentor_id', 'name', 'phone_number','username', 'password', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
+
 class MentorUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mentor
         fields = ['image', 'name', 'phone_number', 'password', 'branch', 'semester', 'codechefID', 'codeforcesID', 'leetcodeID', 'gfgID', 'hackerrankID', 'linkedinID']
 
 class TeamSerializer(serializers.ModelSerializer):
-   
+    team_members = MenteeGetSerializer(many=True, read_only=True)
+    alloted_mentor = MentorGetSerializer(read_only=True)
     class Meta:
         model = Team
         fields = '__all__'
-    team_members = MenteeSerializer(many=True, read_only=True)
-    alloted_mentor=MentorSerializer(read_only=True)
 
 #data of mentees to be displayed after submission of question
 class MenteeQuestion(serializers.ModelSerializer):
